02fe58b3efe9b001f23d70d14cc56057a30b3dfb
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-18 14:08:55 -0500

    primer commit

---
+++ microservicio-usar-cuentas/src/main/java/nttdata/bootcamp/microservicios/usar/cuentas/MicroservicioUsarCuentasApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.usar.cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioUsarCuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioUsarCuentasApplication.class, args);
+	}
+
+}
---
+++ microservicio-usar-cuentas/src/test/java/nttdata/bootcamp/microservicios/usar/cuentas/MicroservicioUsarCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.usar.cuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsarCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
